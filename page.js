91fae3ee458e0a7dd56bd3c44bbf282a914d3977
edwino <edwinobig@gmail.com>
2023-11-06 21:15:42 +0100

    first commit

---
+++ page.js
@@ -0,0 +1,32 @@
+/*var total = 0;
+var factor = 5;
+var result =42;
+var affiche;
+
+function compute(base, factor){
+    result = base*factor;
+    factor*=2;
+    var total= result + factor;
+    return total;
+}
+affiche+='compute(5,4) = ' + compute(5,4);
+affiche+='\ntotal = ' + total + '\n factor =' + factor +'\n result =' +result;
+alert(affiche); *//*
+var dt=new Date;
+var jour = dt.getDay();
+var numero = dt.getDate();
+var mois = dt.getMonth();
+
+var tabjour= new Array("Dimanche", "Lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi");
+var tabmois = new Array("janvier", "fevrire", "mars", "avril", "mai", "juin",
+    "juillet", "aout", "septembre", "octobre","novembre", "decembre");
+document.write(tabjour[jour], " ",numero ," ",tabmois[mois]);
+
+function volumesphere(rayon) {
+    return 4/3*Math.PI*Math.pow(rayon, 3);
+}
+function calculerprix(prixU, nbr_article) {
+    return prixU*nbr_article;
+}
+document.write("payer" + calculerprix(150,4));
+document.write("<br>volume" + volumesphere(1))*/
